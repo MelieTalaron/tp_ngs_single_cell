361b7ad622f87667ec04ed1e2eaca165dc5644e1
Mélie Talaron <melie.talaron@ens-lyon.fr>
2020-11-17 16:25:24 +0000

    Corrected paths (data2 to data) + comments

--- fastq-c_sorting.sh
+++ fastq-c_sorting.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Create target files
-data="/ifb/data/mydatalocal/data2/"
+# Creates target files
+data="/ifb/data/mydatalocal/data/"
 mkdir -p $data
 cd $data
 mkdir -p fastqc_sorting
 
 
-# Work in file sra_data
+#Works in file sra_data
 cd /ifb/data/mydatalocal/data/sra_data
 
-SRR=`ls /ifb/data/mydatalocal/data/sra_data|head -10`
+#Takes the 10th first cells data
+#SRR=`ls /ifb/data/mydatalocal/data/sra_data|head -10`
   
-# Fastq-c results in target file
+#Fastq-c results in target file
 for srr in $SRR
 do
 echo $srr
