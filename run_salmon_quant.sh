2d4777cde60b6ad12a3af91c4041adf842a240e8
Mélie Talaron <melie.talaron@ens-lyon.fr>
2020-11-18 14:10:03 +0000

    Added comments

--- run_salmon_quant.sh
+++ run_salmon_quant.sh
@@ -9,10 +9,12 @@ mkdir -p alignments
 #Works in file index_reference
 cd /ifb/data/mydatalocal/data/alignments
 
+#Creates variables to take the clear data
 SRR=`ls /ifb/data/mydatalocal/data/trimmo_cleared`
+#Creates variables to take the index
 transcriptome_index="/ifb/data/mydatalocal/data/index_reference/mus_musculus_index"
   
-# Trimmo results in target file
+# Quant results in target file
 for srr in $SRR
 do
 echo $srr
